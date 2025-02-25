cc3a2a3c926f0971472ea88f50d68a65acedd031
andirahim <burcifiqi@gamil.com>
2025-02-25 12:54:17 +0300

    commit
    to display  welcome to deuhops

---
+++ FG/src/Main.java
@@ -0,0 +1,11 @@
+public class Protect {
+    public static void main(String[] args) {
+        printWelcomeMessage();
+    }
+
+    public static void printWelcomeMessage() {
+        System.out.println("==============================");
+        System.out.println("****** WELCOME to Deuhops ******");
+        System.out.println("==============================");
+    }
+}
